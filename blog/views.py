9186396f66765c0220583695732a9ec829486ea6
Joel Mbiye <joembiye@gmail.com>
2023-12-30 11:15:54 +0200

    use classes to implement views instead of functions

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,29 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
-def post_list(request): # request is an HttpRequest object
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3) # 3 posts in each page
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer, deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3 # 3 posts in each page
+    template_name = 'blog/post/list.html'
+
+# def post_list(request): # request is an HttpRequest object
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3) # 3 posts in each page
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer, deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, 
