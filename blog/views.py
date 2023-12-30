0af43bd9abd5a3660c8f180a5c2ea1cb035dc5f6
Joel Mbiye <joembiye@gmail.com>
2023-12-30 17:25:33 +0200

    implemented post sharing via email

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from .models import Post
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 class PostListView(ListView):
@@ -37,3 +39,31 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                     {'post': post})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read "\
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n"\
+                      f"{cd['name']}'s comments: {cd['comments']}"
+            send_mail(subject, message, 'joembiye@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,
+                  'blog/post/share.html',
+                  {'post': post,
+                  'form': form,
+                  'sent': sent}) 
